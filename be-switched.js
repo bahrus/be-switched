bcf9802de6cf837000ebafbb60b1e0ec34fb0839
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-16 08:02:44 -0400

    ...

--- be-switched.js
+++ be-switched.js
@@ -1,143 +1,165 @@
-import { XtalDecor } from 'xtal-decor/xtal-decor.js';
-import { CE } from 'trans-render/lib/CE.js';
-import { insertAdjacentTemplate } from 'trans-render/lib/insertAdjacentTemplate.js';
+import { define } from 'be-decorated/be-decorated.js';
 import { getElementToObserve, addListener } from 'be-observant/be-observant.js';
-const ce = new CE({
+import { insertAdjacentTemplate } from 'trans-render/lib/insertAdjacentTemplate.js';
+export class BeSwitchedController {
+    intro(proxy, target, beDecorProps) {
+    }
+    onLHS({ lhs, proxy }) {
+        switch (typeof lhs) {
+            case 'object':
+                const observeParams = lhs;
+                const elementToObserve = getElementToObserve(proxy, observeParams);
+                if (elementToObserve === null) {
+                    console.warn({ msg: '404', observeParams });
+                    return;
+                }
+                addListener(elementToObserve, observeParams, 'lhsVal', proxy);
+                break;
+            default:
+                proxy.lhsVal = lhs;
+        }
+    }
+    onRHS({ rhs, proxy }) {
+        switch (typeof rhs) {
+            case 'object':
+                const observeParams = rhs;
+                const elementToObserve = getElementToObserve(proxy, observeParams);
+                if (elementToObserve === null) {
+                    console.warn({ msg: '404', observeParams });
+                    return;
+                }
+                addListener(elementToObserve, observeParams, 'rhsVal', proxy);
+                break;
+            default:
+                proxy.rhsVal = rhs;
+        }
+    }
+    onIff({ iff, proxy }) {
+        switch (typeof iff) {
+            case 'object':
+                const observeParams = iff;
+                const elementToObserve = getElementToObserve(proxy, observeParams);
+                if (elementToObserve === null) {
+                    console.warn({ msg: '404', observeParams });
+                    return;
+                }
+                addListener(elementToObserve, observeParams, 'iffVal', proxy);
+                break;
+            default:
+                proxy.iffVal = iff;
+        }
+    }
+    calcVal({ iffVal, lhsVal, rhsVal, op, proxy }) {
+        if (!iffVal) {
+            proxy.val = false;
+            return;
+        }
+        if (op === undefined) {
+            proxy.val = true;
+            return;
+        }
+        switch (op) {
+            case '===':
+                proxy.val = (lhsVal === rhsVal);
+                break;
+        }
+    }
+    onVal({ val, proxy }) {
+        setTimeout(() => {
+            if (proxy.echoVal !== proxy.val) {
+                proxy.echoVal = proxy.val;
+            }
+        }, 16);
+    }
+    doMain({ val, echoVal, proxy }) {
+        if (val !== echoVal)
+            return;
+        if (val) {
+            if (proxy.dataset.cnt === undefined) {
+                const appendedChildren = insertAdjacentTemplate(proxy, proxy, 'afterend');
+                addStyle(proxy);
+                proxy.dataset.cnt = appendedChildren.length.toString();
+            }
+            else {
+                const cnt = Number(proxy.dataset.cnt);
+                let nextSib = proxy;
+                let idx = 0;
+                while (nextSib && idx < cnt) {
+                    nextSib = nextSib.nextElementSibling;
+                    if (nextSib)
+                        nextSib.classList.remove('be-switched-hide');
+                    idx++;
+                }
+            }
+        }
+        else {
+            if (proxy.dataset.cnt !== undefined) {
+                const cnt = Number(proxy.dataset.cnt);
+                let nextSib = proxy;
+                let idx = 0;
+                while (nextSib && idx < cnt) {
+                    nextSib = nextSib.nextElementSibling;
+                    if (nextSib)
+                        nextSib.classList.add('be-switched-hide');
+                    idx++;
+                }
+            }
+        }
+    }
+    finale(proxy, target, beDecorProps) {
+        const eventHandlers = proxy.eventHandlers;
+        for (const eh of eventHandlers) {
+            eh.elementToObserve.removeEventListener(eh.on, eh.fn);
+        }
+    }
+}
+const tagName = 'be-switched';
+define({
     config: {
-        tagName: 'be-switched',
+        tagName,
         propDefaults: {
             upgrade: 'template',
             ifWantsToBe: 'switched',
             forceVisible: true,
-            virtualProps: ['eventHandlers', 'iff', 'iffVal', 'lhs', 'op', 'rhs', 'lhsVal', 'rhsVal', 'val', 'echoVal', 'hiddenStyle']
+            virtualProps: ['eventHandlers', 'iff', 'iffVal', 'lhs', 'op', 'rhs', 'lhsVal', 'rhsVal', 'val', 'echoVal', 'hiddenStyle'],
+            intro: 'intro',
+            finale: 'finale'
+        },
+        actions: {
+            onLHS: {
+                ifAllOf: ['lhs']
+            },
+            onRHS: {
+                ifAllOf: ['rhs']
+            },
+            onIff: {
+                ifAllOf: ['iff']
+            },
+            calcVal: {
+                ifKeyIn: ['iffVal', 'lhsVal', 'rhsVal', 'op']
+            },
+            onVal: {
+                ifKeyIn: ['val']
+            },
+            doMain: {
+                ifKeyIn: ['val', 'echoVal']
+            }
         }
     },
     complexPropDefaults: {
-        actions: [
-            ({ lhs, self }) => {
-                switch (typeof lhs) {
-                    case 'object':
-                        const observeParams = lhs;
-                        const elementToObserve = getElementToObserve(self, observeParams);
-                        if (elementToObserve === null) {
-                            console.warn({ msg: '404', observeParams });
-                            return;
-                        }
-                        addListener(elementToObserve, observeParams, 'lhsVal', self);
-                        break;
-                    default:
-                        self.lhsVal = lhs;
-                }
-            },
-            ({ rhs, self }) => {
-                switch (typeof rhs) {
-                    case 'object':
-                        const observeParams = rhs;
-                        const elementToObserve = getElementToObserve(self, observeParams);
-                        if (elementToObserve === null) {
-                            console.warn({ msg: '404', observeParams });
-                            return;
-                        }
-                        addListener(elementToObserve, observeParams, 'rhsVal', self);
-                        break;
-                    default:
-                        self.rhsVal = rhs;
-                }
-            },
-            ({ iff, self }) => {
-                switch (typeof iff) {
-                    case 'object':
-                        const observeParams = iff;
-                        const elementToObserve = getElementToObserve(self, observeParams);
-                        if (elementToObserve === null) {
-                            console.warn({ msg: '404', observeParams });
-                            return;
-                        }
-                        addListener(elementToObserve, observeParams, 'iffVal', self);
-                        break;
-                    default:
-                        self.iffVal = iff;
-                }
-            },
-            ({ iffVal, lhsVal, rhsVal, op, self }) => {
-                if (!iffVal) {
-                    self.val = false;
-                    return;
-                }
-                if (op === undefined) {
-                    self.val = true;
-                    return;
-                }
-                switch (op) {
-                    case '===':
-                        self.val = (lhsVal === rhsVal);
-                        break;
-                }
-            },
-            ({ val, self }) => {
-                setTimeout(() => {
-                    if (self.echoVal !== self.val) {
-                        self.echoVal = self.val;
-                    }
-                }, 16);
-            },
-            ({ val, echoVal, self }) => {
-                if (val !== echoVal)
-                    return;
-                if (val) {
-                    if (self.dataset.cnt === undefined) {
-                        const appendedChildren = insertAdjacentTemplate(self, self, 'afterend');
-                        addStyle(self);
-                        self.dataset.cnt = appendedChildren.length.toString();
-                    }
-                    else {
-                        const cnt = Number(self.dataset.cnt);
-                        let nextSib = self;
-                        let idx = 0;
-                        while (nextSib && idx < cnt) {
-                            nextSib = nextSib.nextElementSibling;
-                            nextSib.classList.remove('be-switched-hide');
-                            idx++;
-                        }
-                    }
-                }
-                else {
-                    if (self.dataset.cnt !== undefined) {
-                        const cnt = Number(self.dataset.cnt);
-                        let nextSib = self;
-                        let idx = 0;
-                        while (nextSib && idx < cnt) {
-                            nextSib = nextSib.nextElementSibling;
-                            nextSib.classList.add('be-switched-hide');
-                            idx++;
-                        }
-                    }
-                }
-            }
-        ],
-        on: {},
-        init: (self, decor) => {
-        },
-        finale: (self, target) => {
-            const eventHandlers = self.eventHandlers;
-            for (const eh of eventHandlers) {
-                eh.elementToObserve.removeEventListener(eh.onz, eh.fn);
-            }
-        }
-    },
-    superclass: XtalDecor
+        controller: BeSwitchedController
+    }
 });
-document.head.appendChild(document.createElement('be-switched'));
 const styleMap = new WeakSet();
-function addStyle({ self }) {
-    let rootNode = self.getRootNode();
+function addStyle(proxy) {
+    let rootNode = proxy.getRootNode();
     if (rootNode.host === undefined) {
         rootNode = document.head;
     }
     if (!styleMap.has(rootNode)) {
         styleMap.add(rootNode);
         const style = document.createElement('style');
-        const hiddenStyle = self.hiddenStyle || 'display:none';
+        const hiddenStyle = proxy.hiddenStyle || 'display:none';
         style.innerHTML = /* css */ `
             .be-switched-hide{
                 ${hiddenStyle}
@@ -146,3 +168,4 @@ function addStyle({ self }) {
         rootNode.appendChild(style);
     }
 }
+document.head.appendChild(document.createElement(tagName));
--- be-switched.ts
+++ be-switched.ts
@@ -1,145 +1,177 @@
-import {XtalDecor, XtalDecorCore} from 'xtal-decor/xtal-decor.js';
-import { XtalDecorProps } from 'xtal-decor/types';
-import {CE} from 'trans-render/lib/CE.js';
-import { IObserve } from 'be-observant/types';
+import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
+import {BeSwitchedVirtualProps, BeSwitchedActions, BeSwitchedProps} from './types';
+import {getElementToObserve, addListener, IObserve} from 'be-observant/be-observant.js';
 import { insertAdjacentTemplate } from 'trans-render/lib/insertAdjacentTemplate.js';
-import {getElementToObserve, addListener} from 'be-observant/be-observant.js';
 
-const ce = new CE<XtalDecorCore<Element>>({
+export class BeSwitchedController implements BeSwitchedActions{
+
+    intro(proxy: Element & BeSwitchedVirtualProps, target: Element, beDecorProps: BeDecoratedProps){
+
+    }
+
+    onLHS({lhs, proxy}: this){
+        switch(typeof lhs){
+            case 'object':
+                const observeParams = lhs as IObserve;
+                const elementToObserve = getElementToObserve(proxy, observeParams);
+                if(elementToObserve === null){
+                    console.warn({msg:'404',observeParams});
+                    return;
+                }
+                addListener(elementToObserve, observeParams, 'lhsVal', proxy);
+                break;
+            default:
+                proxy.lhsVal = lhs;
+        }        
+    }
+
+    onRHS({rhs, proxy}: this){
+        switch(typeof rhs){
+            case 'object':
+                const observeParams = rhs as IObserve;
+                const elementToObserve = getElementToObserve(proxy, observeParams);
+                if(elementToObserve === null){
+                    console.warn({msg:'404',observeParams});
+                    return;
+                }
+                addListener(elementToObserve, observeParams, 'rhsVal', proxy);
+                break;
+            default:
+                proxy.rhsVal = rhs;
+        }        
+    }
+
+    onIff({iff, proxy}: this){
+        switch(typeof iff){
+            case 'object':
+                const observeParams = iff as IObserve;
+                const elementToObserve = getElementToObserve(proxy, observeParams);
+                if(elementToObserve === null){
+                    console.warn({msg:'404',observeParams});
+                    return;
+                }
+                addListener(elementToObserve, observeParams, 'iffVal', proxy);
+                break;
+            default:
+                proxy.iffVal = iff;
+        } 
+    }
+
+    calcVal({iffVal, lhsVal, rhsVal, op, proxy}: this){
+        if(!iffVal){
+            proxy.val = false;
+            return;
+        }
+        if(op === undefined) {
+            proxy.val = true;
+            return;
+        }
+        switch(op){
+            case '===':
+                proxy.val = (lhsVal === rhsVal);
+                break;
+        }        
+    }
+
+    onVal({val, proxy}: this){
+        setTimeout(() => {
+            if(proxy.echoVal !== proxy.val){
+                proxy.echoVal = proxy.val;
+            }
+        }, 16);
+    }
+
+    doMain({val, echoVal, proxy}: this){
+        if(val !== echoVal) return;
+        if(val){
+            if(proxy.dataset.cnt === undefined){
+                const appendedChildren = insertAdjacentTemplate(proxy, proxy, 'afterend');
+                addStyle(proxy);
+                proxy.dataset.cnt = appendedChildren.length.toString();
+            }else{
+                const cnt = Number(proxy.dataset.cnt);
+                let nextSib = proxy as Element | null;
+                let idx = 0
+                while(nextSib && idx < cnt){
+                    nextSib = nextSib.nextElementSibling;
+                    if(nextSib) nextSib.classList.remove('be-switched-hide');
+                    idx++;
+                }                        
+            }
+            
+        }else{
+            if(proxy.dataset.cnt !== undefined){
+                const cnt = Number(proxy.dataset.cnt);
+                let nextSib = proxy as Element | null;
+                let idx = 0
+                while(nextSib && idx < cnt){
+                    nextSib = nextSib.nextElementSibling;
+                    if(nextSib) nextSib.classList.add('be-switched-hide');
+                    idx++;
+                }
+            }
+        }
+    }
+
+    finale(proxy: Element & BeSwitchedVirtualProps, target:Element, beDecorProps: BeDecoratedProps){
+        const eventHandlers = proxy.eventHandlers;
+        for(const eh of eventHandlers){
+            eh.elementToObserve.removeEventListener(eh.on, eh.fn);
+        }
+    }
+}
+
+export interface BeSwitchedController extends BeSwitchedProps{}
+
+const tagName = 'be-switched';
+
+define<BeSwitchedProps & BeDecoratedProps<BeSwitchedProps, BeSwitchedActions>, BeSwitchedActions>({
     config:{
-        tagName: 'be-switched',
+        tagName,
         propDefaults:{
             upgrade: 'template',
             ifWantsToBe: 'switched',
             forceVisible: true,
-            virtualProps: ['eventHandlers', 'iff', 'iffVal', 'lhs', 'op', 'rhs', 'lhsVal', 'rhsVal', 'val', 'echoVal', 'hiddenStyle']
-        }
-    },
-    complexPropDefaults: {
-        actions:[
-            ({lhs, self}) => {
-                switch(typeof lhs){
-                    case 'object':
-                        const observeParams = lhs as IObserve;
-                        const elementToObserve = getElementToObserve(self, observeParams);
-                        if(elementToObserve === null){
-                            console.warn({msg:'404',observeParams});
-                            return;
-                        }
-                        addListener(elementToObserve, observeParams, 'lhsVal', self);
-                        break;
-                    default:
-                        self.lhsVal = lhs;
-                }
-            },
-            ({rhs, self}) => {
-                switch(typeof rhs){
-                    case 'object':
-                        const observeParams = rhs as IObserve;
-                        const elementToObserve = getElementToObserve(self, observeParams);
-                        if(elementToObserve === null){
-                            console.warn({msg:'404',observeParams});
-                            return;
-                        }
-                        addListener(elementToObserve, observeParams, 'rhsVal', self);
-                        break;
-                    default:
-                        self.rhsVal = rhs;
-                }                
-            },
-            ({iff, self}) => {
-                switch(typeof iff){
-                    case 'object':
-                        const observeParams = iff as IObserve;
-                        const elementToObserve = getElementToObserve(self, observeParams);
-                        if(elementToObserve === null){
-                            console.warn({msg:'404',observeParams});
-                            return;
-                        }
-                        addListener(elementToObserve, observeParams, 'iffVal', self);
-                        break;
-                    default:
-                        self.iffVal = iff;
-                } 
-            },
-            ({iffVal, lhsVal, rhsVal, op, self}) => {
-                if(!iffVal){
-                    self.val = false;
-                    return;
-                }
-                if(op === undefined) {
-                    self.val = true;
-                    return;
-                }
-                switch(op){
-                    case '===':
-                        self.val = (lhsVal === rhsVal);
-                        break;
-                }
-            },
-            ({val, self}) => {
-                setTimeout(() => {
-                    if(self.echoVal !== self.val){
-                        self.echoVal = self.val;
-                    }
-                }, 16);
-            },
-            ({val, echoVal, self}) => {
-                if(val !== echoVal) return;
-                if(val){
-                    if(self.dataset.cnt === undefined){
-                        const appendedChildren = insertAdjacentTemplate(self, self, 'afterend');
-                        addStyle(self);
-                        self.dataset.cnt = appendedChildren.length.toString();
-                    }else{
-                        const cnt = Number(self.dataset.cnt);
-                        let nextSib = self;
-                        let idx = 0
-                        while(nextSib && idx < cnt){
-                            nextSib = nextSib.nextElementSibling;
-                            nextSib.classList.remove('be-switched-hide');
-                            idx++;
-                        }                        
-                    }
-                    
-                }else{
-                    if(self.dataset.cnt !== undefined){
-                        const cnt = Number(self.dataset.cnt);
-                        let nextSib = self;
-                        let idx = 0
-                        while(nextSib && idx < cnt){
-                            nextSib = nextSib.nextElementSibling;
-                            nextSib.classList.add('be-switched-hide');
-                            idx++;
-                        }
-                    }
-                }
-            }
-        ],
-        on:{},
-        init: (self: any, decor: XtalDecorProps<Element>) => {
+            virtualProps: ['eventHandlers', 'iff', 'iffVal', 'lhs', 'op', 'rhs', 'lhsVal', 'rhsVal', 'val', 'echoVal', 'hiddenStyle'],
+            intro: 'intro',
+            finale: 'finale'
         },
-        finale: (self: Element, target: Element) => {
-            const eventHandlers = (<any>self).eventHandlers;
-            for(const eh of eventHandlers){
-                eh.elementToObserve.removeEventListener(eh.onz, eh.fn);
+        actions:{
+            onLHS:{
+                ifAllOf: ['lhs']
+            },
+            onRHS: {
+                ifAllOf: ['rhs']
+            },
+            onIff: {
+                ifAllOf: ['iff']
+            },
+            calcVal: {
+                ifKeyIn: ['iffVal', 'lhsVal', 'rhsVal', 'op']
+            },
+            onVal: {
+                ifKeyIn: ['val']
+            },
+            doMain:{
+                ifKeyIn: ['val', 'echoVal']
             }
         }
     },
-    superclass: XtalDecor
+    complexPropDefaults:{
+        controller: BeSwitchedController
+    }
 });
-document.head.appendChild(document.createElement('be-switched'));
 
 const styleMap = new WeakSet<Node>();
-function addStyle({self}: any){
-    let rootNode = self.getRootNode();
+function addStyle(proxy: Element & BeSwitchedVirtualProps){
+    let rootNode = proxy.getRootNode();
     if ((<any>rootNode).host === undefined) {
         rootNode = document.head;
     }
     if (!styleMap.has(rootNode)) {
         styleMap.add(rootNode);
         const style = document.createElement('style');
-        const hiddenStyle = self.hiddenStyle || 'display:none';
+        const hiddenStyle = proxy.hiddenStyle || 'display:none';
         style.innerHTML = /* css */`
             .be-switched-hide{
                 ${hiddenStyle}
@@ -149,4 +181,4 @@ function addStyle({self}: any){
     }
 }
 
-
+document.head.appendChild(document.createElement(tagName));
