862b7666818245b0707df3dace398baaf2ec1861
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-22 21:25:13 -0400

    0.0.19

--- be-switched.ts
+++ be-switched.ts
@@ -1,8 +1,8 @@
+import { insertAdjacentTemplate } from 'trans-render/lib/insertAdjacentTemplate.js';
 import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
 import {BeSwitchedVirtualProps, BeSwitchedActions, BeSwitchedProps} from './types';
 import {getElementToObserve, IObserve} from 'be-observant/getElementToObserve.js';
 import {addListener} from 'be-observant/addListener.js';
-import { insertAdjacentTemplate } from 'trans-render/lib/insertAdjacentTemplate.js';
 import {register} from 'be-hive/register.js';
 
 export class BeSwitchedController implements BeSwitchedActions{
