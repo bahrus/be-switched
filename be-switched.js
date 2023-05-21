559aadb0757cae2a4761b2a858f7237cc29be82b
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-05-21 18:58:46 -0400

    0.0.87

--- be-switched.js
+++ be-switched.js
@@ -1,204 +1,99 @@
-import { define } from 'be-decorated/DE.js';
-import { hookUp } from 'be-observant/hookUp.js';
+import { BE, propDefaults, propInfo } from 'be-enhanced/BE.js';
+import { XE } from 'xtal-element/XE.js';
 import { register } from 'be-hive/register.js';
-export class BeSwitchedController extends EventTarget {
-    onLHS({ lhs, proxy }) {
-        hookUp(lhs, proxy, 'lhsVal');
+export class BeSwitched extends BE {
+    static get beConfig() {
+        return {
+            parse: true,
+        };
     }
-    onRHS({ rhs, proxy }) {
-        hookUp(rhs, proxy, 'rhsVal');
+    calcVal(self) {
+        const { lhs, rhs, checkIfNonEmptyArray } = self;
+        if (checkIfNonEmptyArray) {
+            if (!Array.isArray(lhs) || lhs.length === 0)
+                return {
+                    val: false,
+                    resolved: true,
+                };
+        }
+        return {
+            val: lhs === rhs,
+            resolved: true,
+        };
     }
-    onIf({ if: iff, proxy }) {
-        hookUp(iff, proxy, 'ifVal');
-    }
-    onIfNonEmptyArray({ ifNonEmptyArray, proxy }) {
-        if (Array.isArray(ifNonEmptyArray)) {
-            proxy.ifNonEmptyArrayVal = ifNonEmptyArray;
+    async onTrue(self) {
+        const { enhancedElement, toggleDisabled, deferRendering } = self;
+        const itemref = enhancedElement.getAttribute('itemref');
+        if (itemref === null) {
+            const keys = [];
+            const { insertAdjacentTemplate } = await import('trans-render/lib/insertAdjacentTemplate.js');
+            const appendedChildren = insertAdjacentTemplate(enhancedElement, enhancedElement, 'afterend');
+            for (const child of appendedChildren) {
+                if (!child.id) {
+                    child.id = 'a' + crypto.randomUUID();
+                }
+                keys.push(child.id);
+            }
+            enhancedElement.setAttribute('itemref', keys.join(' '));
         }
         else {
-            hookUp(ifNonEmptyArray, proxy, 'ifNonEmptyArrayVal');
+            if (deferRendering) {
+                self.deferRendering = false;
+                return;
+            }
+            const rn = enhancedElement.getRootNode();
+            const keys = itemref.split(' ');
+            for (const key of keys) {
+                const child = rn.getElementById(key);
+                if (child === null)
+                    continue;
+                child.classList.remove('be-switched-hide');
+                if (toggleDisabled && child.disabled === false) {
+                    child.disabled = true;
+                }
+            }
         }
     }
-    chkMedia({}, e) {
-        return { matchesMediaQuery: e.matches };
+    async onFalse(self) {
+        const { enhancedElement, toggleDisabled } = self;
+        const itemref = enhancedElement.getAttribute('itemref');
+        if (itemref === null)
+            return;
+        addStyle(self);
+        const rn = enhancedElement.getRootNode();
+        const keys = itemref.split(' ');
+        for (const key of keys) {
+            const child = rn.getElementById(key);
+            if (child === null)
+                continue;
+            child.classList.add('be-switched-hide');
+            if (toggleDisabled && child.disabled === false) {
+                child.disabled = true;
+            }
+        }
     }
     #mql;
-    addMediaListener(pp) {
-        const { ifMediaMatches } = pp;
+    addMediaListener(self) {
+        const { ifMediaMatches, enhancedElement } = self;
         if (!ifMediaMatches)
             return [{}, {}]; //clears previous listener
         this.#mql = window.matchMedia(ifMediaMatches); //TODO:  support observant media matches
         return [{}, { chkMedia: { on: 'change', of: this.#mql, doInit: true } }];
     }
-    calcVal(pp) {
-        const { deferRendering } = pp;
-        if (deferRendering) {
-            return {
-                deferRendering: false,
-            };
-        }
-        const { ifVal, lhsVal, rhsVal } = pp;
-        if (!ifVal) {
-            return {
-                val: false,
-            };
-        }
-        const { ifMediaMatches, matchesMediaQuery } = pp;
-        if (ifMediaMatches !== undefined) {
-            if (!matchesMediaQuery) {
-                return {
-                    val: false,
-                };
-            }
-        }
-        const { ifNonEmptyArray, ifNonEmptyArrayVal } = pp;
-        if (ifNonEmptyArray !== undefined) {
-            if (ifNonEmptyArrayVal === undefined || ifNonEmptyArrayVal.length === 0) {
-                return {
-                    val: false
-                };
-            }
-        }
-        const { op } = pp;
-        if (op === undefined) {
-            return {
-                val: true
-            };
-        }
-        switch (op) {
-            case '===':
-                return { val: (lhsVal === rhsVal) };
-        }
-    }
-    #echoTimeOut;
-    onVal({ val, proxy, displayDelay }) {
-        if (proxy.debug) {
-            console.log({ val, proxy });
-        }
-        clearTimeout(this.#echoTimeOut);
-        this.#echoTimeOut = setTimeout(() => {
-            if (proxy.echoVal !== proxy.val) {
-                proxy.echoVal = proxy.val;
-            }
-        }, displayDelay);
-    }
-    async doMain({ val, echoVal, proxy, toggleDisabled }) {
-        if (val !== echoVal)
-            return;
-        if (val) {
-            if (proxy.dataset.cnt === undefined) {
-                const { insertAdjacentTemplate } = await import('trans-render/lib/insertAdjacentTemplate.js');
-                const appendedChildren = insertAdjacentTemplate(proxy, proxy, 'afterend');
-                proxy.dataset.cnt = (appendedChildren.length + 1).toString();
-            }
-            else {
-                const cnt = Number(proxy.dataset.cnt);
-                let nextSib = proxy;
-                let idx = 1;
-                while (nextSib && idx < cnt) {
-                    nextSib = nextSib.nextElementSibling;
-                    if (nextSib) {
-                        nextSib.classList.remove('be-switched-hide');
-                        if (toggleDisabled && nextSib.disabled === false) {
-                            nextSib.disabled = true;
-                        }
-                    }
-                    idx++;
-                }
-            }
-        }
-        else {
-            addStyle(proxy);
-            //checks for !val are done when appropriate -- boolean conditions, not necessarily lazy loading, is now false
-            // if(setClass !== undefined && !val){
-            //     proxy.classList.remove(setClass);
-            // }
-            if (proxy.dataset.cnt !== undefined) {
-                const cnt = Number(proxy.dataset.cnt);
-                let nextSib = proxy;
-                let idx = 1;
-                while (nextSib && idx < cnt) {
-                    nextSib = nextSib.nextElementSibling;
-                    if (nextSib !== null) {
-                        if (!val)
-                            nextSib.classList.add('be-switched-hide');
-                        if (toggleDisabled && nextSib.disabled === true) {
-                            nextSib.disabled = false;
-                        }
-                        idx++;
-                    }
-                }
-            }
-        }
-    }
-    async finale(proxy, target, beDecorProps) {
-        const { unsubscribe } = await import('trans-render/lib/subscribe.js');
-        unsubscribe(proxy);
-        const eventHandlers = proxy.eventHandlers;
-        for (const eh of eventHandlers) {
-            eh.elementToObserve.removeEventListener(eh.on, eh.fn);
-        }
+    chkMedia(self, e) {
+        return { matchesMediaQuery: e.matches };
     }
 }
-const tagName = 'be-switched';
-const upgrade = 'template';
-define({
-    config: {
-        tagName,
-        propDefaults: {
-            forceVisible: [upgrade],
-            virtualProps: [
-                'eventHandlers', 'if', 'ifVal', 'lhs', 'op', 'rhs', 'lhsVal', 'rhsVal',
-                'val', 'echoVal', 'hiddenStyle', 'ifMediaMatches', 'matchesMediaQuery',
-                'ifNonEmptyArray', 'ifNonEmptyArrayVal', 'displayDelay', 'beOosoom', 'disabled',
-            ],
-            proxyPropDefaults: {
-                displayDelay: 16,
-                op: '===',
-                beOosoom: '!disabled',
-            },
-            finale: 'finale',
-        },
-        actions: {
-            onLHS: {
-                ifKeyIn: ['lhs']
-            },
-            onRHS: {
-                ifKeyIn: ['rhs']
-            },
-            onIf: {
-                ifKeyIn: ['if']
-            },
-            addMediaListener: {
-                ifKeyIn: ['ifMediaMatches']
-            },
-            onIfNonEmptyArray: 'ifNonEmptyArray',
-            calcVal: {
-                ifKeyIn: ['ifVal', 'lhsVal', 'rhsVal', 'op', 'matchesMediaQuery', 'ifNonEmptyArrayVal', 'echoVal'],
-                ifNoneOf: ['disabled']
-            },
-            onVal: {
-                ifKeyIn: ['val']
-            },
-            doMain: {
-                ifKeyIn: ['val', 'echoVal']
-            },
-        }
-    },
-    complexPropDefaults: {
-        controller: BeSwitchedController
-    }
-});
 const styleMap = new WeakSet();
-function addStyle(proxy) {
-    let rootNode = proxy.getRootNode();
+function addStyle(ap) {
+    const { enhancedElement, hiddenStyle } = ap;
+    let rootNode = enhancedElement.getRootNode();
     if (rootNode.host === undefined) {
         rootNode = document.head;
     }
     if (!styleMap.has(rootNode)) {
         styleMap.add(rootNode);
         const style = document.createElement('style');
-        const hiddenStyle = proxy.hiddenStyle || 'display:none';
         style.innerHTML = /* css */ `
             .be-switched-hide{
                 ${hiddenStyle}
@@ -207,4 +102,53 @@ function addStyle(proxy) {
         rootNode.appendChild(style);
     }
 }
-register('switched', upgrade, tagName);
+const tagName = 'be-switched';
+const ifWantsToBe = 'switched';
+const upgrade = '*';
+const xe = new XE({
+    config: {
+        tagName,
+        propDefaults: {
+            ...propDefaults,
+            val: false,
+            echoVal: false,
+            lhs: false,
+            rhs: true,
+            displayDelay: 16,
+            op: '===',
+            checkIfNonEmptyArray: false,
+            ifMediaMatches: '',
+            hiddenStyle: 'display:none',
+            toggleDisabled: false,
+        },
+        propInfo: {
+            ...propInfo,
+            val: {
+                notify: {
+                    echoTo: {
+                        key: 'echoVal',
+                        delay: 'displayDelay'
+                    }
+                }
+            }
+        },
+        actions: {
+            calcVal: {
+                ifKeyIn: ['lhs', 'rhs']
+            },
+            onTrue: {
+                ifEquals: ['val', 'echoVal'],
+                ifAllOf: ['val']
+            },
+            onFalse: {
+                ifEquals: ['val', 'echoVal'],
+                ifNoneOf: ['val']
+            },
+            addMediaListener: {
+                ifKeyIn: ['ifMediaMatches']
+            }
+        }
+    },
+    superclass: BeSwitched
+});
+register(ifWantsToBe, upgrade, tagName);
--- be-switched.ts
+++ be-switched.ts
@@ -1,223 +1,103 @@
-import {define, BeDecoratedProps} from 'be-decorated/DE.js';
-import {VirtualProps, Actions, PP, Proxy, PPP, PPE} from './types';
-import {hookUp} from 'be-observant/hookUp.js';
+import {BE, propDefaults, propInfo} from 'be-enhanced/BE.js';
+import {BEConfig} from 'be-enhanced/types';
+import {XE} from 'xtal-element/XE.js';
+import {Actions, AllProps, AP, PAP, ProPAP, POA} from './v2/types';
 import {register} from 'be-hive/register.js';
+import { IEchoTo } from '../xtal-element/types';
 
-export class BeSwitchedController extends EventTarget implements Actions{
-
-
-    onLHS({lhs, proxy}: PP){
-        hookUp(lhs, proxy, 'lhsVal');       
+export class BeSwitched extends BE<AP, Actions, HTMLTemplateElement> implements Actions{
+    static  override get beConfig(){
+        return {
+            parse: true,
+        } as BEConfig
     }
 
-    onRHS({rhs, proxy}: PP){
-        hookUp(rhs, proxy, 'rhsVal');        
-    }
-
-    onIf({if: iff, proxy}: PP){
-        hookUp(iff, proxy, 'ifVal');
-    }
-
-    onIfNonEmptyArray({ifNonEmptyArray, proxy}: PP){
-        if(Array.isArray(ifNonEmptyArray)){
-            proxy.ifNonEmptyArrayVal = ifNonEmptyArray;
-        }else{
-            hookUp(ifNonEmptyArray, proxy, 'ifNonEmptyArrayVal');
-        }
-    }
-
-    chkMedia({}: PP, e: MediaQueryListEvent){
-        return {matchesMediaQuery: e.matches} as PPP;
-    }
-    #mql: MediaQueryList | undefined;
-    addMediaListener(pp: PP){
-        const { ifMediaMatches} = pp;
-        if(!ifMediaMatches) return [{}, {}] as PPE; //clears previous listener
-        this.#mql = window.matchMedia(ifMediaMatches! as string); //TODO:  support observant media matches
-        return [{}, {chkMedia: {on: 'change', of: this.#mql, doInit: true}}] as PPE;
-    }
-
-    calcVal(pp: PP){
-        const {deferRendering} = pp;
-        if(deferRendering){
-            return {
-                deferRendering: false,
-            }
-        }
-        const {ifVal, lhsVal, rhsVal} = pp;
-        if(!ifVal){
-            return {
+    calcVal(self: this): PAP {
+        const {lhs, rhs, checkIfNonEmptyArray} = self;
+        if(checkIfNonEmptyArray){
+            if(!Array.isArray(lhs) || lhs.length === 0) return {
                 val: false,
+                resolved: true,
             }
         }
-        const {ifMediaMatches, matchesMediaQuery} = pp;
-        if(ifMediaMatches !== undefined){
-            if(!matchesMediaQuery){
-                return {
-                    val: false,
+        return {
+            val: lhs === rhs,
+            resolved: true,
+        }
+    }
+
+    async onTrue(self: this) {
+        const {enhancedElement, toggleDisabled, deferRendering} = self;
+        const itemref= enhancedElement.getAttribute('itemref');
+        if(itemref === null){
+            const keys : string[] = [];
+            const {insertAdjacentTemplate} = await import('trans-render/lib/insertAdjacentTemplate.js');
+            const appendedChildren = insertAdjacentTemplate(enhancedElement, enhancedElement, 'afterend');
+            for(const child of appendedChildren){
+                if(!child.id){
+                    child.id = 'a' + crypto.randomUUID();
                 }
+                keys.push(child.id);
             }
-        }
-        const {ifNonEmptyArray, ifNonEmptyArrayVal} = pp;
-        if(ifNonEmptyArray !== undefined){
-            if(ifNonEmptyArrayVal === undefined || ifNonEmptyArrayVal.length === 0){
-                return {
-                    val: false
-                };
-            }
-        }
-        const {op} =  pp;
-        if(op === undefined) {
-            return {
-                val: true
-            };
-        }
-
-        switch(op){
-            case '===':
-                return {val: (lhsVal === rhsVal)};
-
-        }        
-    }
-
-    #echoTimeOut: string | number | NodeJS.Timeout | undefined;
-    onVal({val, proxy, displayDelay}: PP){
-        if((<any>proxy).debug){
-            console.log({val, proxy});
-        }
-        clearTimeout(this.#echoTimeOut);
-        this.#echoTimeOut = setTimeout(() => {
-            if(proxy.echoVal !== proxy.val){
-                proxy.echoVal = proxy.val;
-            }
-        }, displayDelay);
-    }
-
-    async doMain({val, echoVal, proxy, toggleDisabled}: PP){
-        if(val !== echoVal) return;
-        if(val){
-            if(proxy.dataset.cnt === undefined){
-                const {insertAdjacentTemplate} = await import('trans-render/lib/insertAdjacentTemplate.js');
-                const appendedChildren = insertAdjacentTemplate(proxy, proxy, 'afterend');
-                
-                proxy.dataset.cnt = (appendedChildren.length + 1).toString();
-            }else{
-                const cnt = Number(proxy.dataset.cnt);
-                let nextSib = proxy as Element | null;
-                let idx = 1
-                while(nextSib && idx < cnt){
-                    nextSib = nextSib.nextElementSibling;
-                    if(nextSib) {
-                        nextSib.classList.remove('be-switched-hide');
-                        if(toggleDisabled && (<any>nextSib).disabled === false){
-                            (<any>nextSib).disabled = true;
-                        }
-
-                    }
-                    idx++;
-                }                        
-            }
-            
+            enhancedElement.setAttribute('itemref', keys.join(' '));
         }else{
-            addStyle(proxy);
-            //checks for !val are done when appropriate -- boolean conditions, not necessarily lazy loading, is now false
-            // if(setClass !== undefined && !val){
-            //     proxy.classList.remove(setClass);
-            // }
-            if(proxy.dataset.cnt !== undefined){
-                const cnt = Number(proxy.dataset.cnt);
-                let nextSib = proxy as Element | null;
-                let idx = 1
-                while(nextSib && idx < cnt){
-                    nextSib = nextSib.nextElementSibling;
-                    if(nextSib !== null){
-                        if(!val) nextSib.classList.add('be-switched-hide');
-                        if(toggleDisabled && (<any>nextSib).disabled === true){
-                            (<any>nextSib).disabled = false;
-                        }
-                        idx++;
-                    }
-
+            if(deferRendering){
+                self.deferRendering = false;
+                return;
+            }
+            const rn = enhancedElement.getRootNode() as DocumentFragment;
+            const keys = itemref.split(' ');
+            for(const key of keys){
+                const child = rn.getElementById(key);
+                if(child === null) continue;
+                child.classList.remove('be-switched-hide');
+                if(toggleDisabled && (<any>child).disabled === false){
+                    (<any>child).disabled = true;
                 }
             }
-
+        }
+    }
+    async onFalse(self: this){
+        const {enhancedElement, toggleDisabled} = self;
+        const itemref = enhancedElement.getAttribute('itemref');
+        if(itemref === null) return;
+        addStyle(self);
+        const rn = enhancedElement.getRootNode() as DocumentFragment;
+        const keys = itemref.split(' ');
+        for(const key of keys){
+            const child = rn.getElementById(key);
+            if(child === null) continue;
+            child.classList.add('be-switched-hide');
+            if(toggleDisabled && (<any>child).disabled === false){
+                (<any>child).disabled = true;
+            }
         }
     }
 
+    #mql: MediaQueryList | undefined;
+    addMediaListener(self: this){
+        const { ifMediaMatches, enhancedElement} = self;
+        if(!ifMediaMatches) return [{}, {}] as POA; //clears previous listener
+        this.#mql = window.matchMedia(ifMediaMatches! as string); //TODO:  support observant media matches
+        return [{}, {chkMedia: {on: 'change', of: this.#mql, doInit: true}}] as POA;
+    }
 
-    async finale(proxy: Proxy, target:Element, beDecorProps: BeDecoratedProps){
-        const {unsubscribe} = await import('trans-render/lib/subscribe.js');
-        unsubscribe(proxy);
-        const eventHandlers = proxy.eventHandlers;
-        for(const eh of eventHandlers){
-            eh.elementToObserve.removeEventListener(eh.on, eh.fn);
-        }
+    chkMedia(self: this, e: MediaQueryListEvent){
+        return {matchesMediaQuery: e.matches} as PAP;
     }
 }
 
-
-const tagName = 'be-switched';
-
-const upgrade = 'template';
-
-define<Proxy & BeDecoratedProps<Proxy, Actions>, Actions>({
-    config:{
-        tagName,
-        propDefaults:{
-            forceVisible: [upgrade],
-            virtualProps: [
-                'eventHandlers', 'if', 'ifVal', 'lhs', 'op', 'rhs', 'lhsVal', 'rhsVal', 
-                'val', 'echoVal', 'hiddenStyle', 'ifMediaMatches', 'matchesMediaQuery',
-                'ifNonEmptyArray', 'ifNonEmptyArrayVal', 'displayDelay', 'beOosoom', 'disabled',
-            ],
-            proxyPropDefaults:{
-                displayDelay: 16,
-                op: '===',
-                beOosoom: '!disabled',
-            },
-            finale: 'finale',
-        },
-        actions:{
-            onLHS:{
-                ifKeyIn: ['lhs']
-            },
-            onRHS: {
-                ifKeyIn: ['rhs']
-            },
-            onIf: {
-                ifKeyIn: ['if']
-            },
-            addMediaListener: {
-                ifKeyIn: ['ifMediaMatches']
-            },
-            onIfNonEmptyArray:'ifNonEmptyArray',
-            calcVal: {
-                ifKeyIn: ['ifVal', 'lhsVal', 'rhsVal', 'op', 'matchesMediaQuery', 'ifNonEmptyArrayVal', 'echoVal'],
-                ifNoneOf: ['disabled']
-            },
-            onVal: {
-                ifKeyIn: ['val']
-            },
-            doMain:{
-                ifKeyIn: ['val', 'echoVal']
-            },
-        }
-    },
-    complexPropDefaults:{
-        controller: BeSwitchedController
-    }
-});
-
 const styleMap = new WeakSet<Node>();
-function addStyle(proxy: Element & VirtualProps){
-    let rootNode = proxy.getRootNode();
+
+function addStyle(ap: AP){
+    const {enhancedElement, hiddenStyle} = ap;
+    let rootNode = enhancedElement.getRootNode();
     if ((<any>rootNode).host === undefined) {
         rootNode = document.head;
     }
     if (!styleMap.has(rootNode)) {
         styleMap.add(rootNode);
         const style = document.createElement('style');
-        const hiddenStyle = proxy.hiddenStyle || 'display:none';
         style.innerHTML = /* css */`
             .be-switched-hide{
                 ${hiddenStyle}
@@ -226,5 +106,58 @@ function addStyle(proxy: Element & VirtualProps){
         rootNode.appendChild(style);
     }
 }
-register('switched', upgrade, tagName);
 
+export interface BeSwitched extends AllProps{}
+
+const tagName = 'be-switched';
+const ifWantsToBe = 'switched';
+const upgrade = '*';
+
+const xe = new XE<AP, Actions>({
+    config: {
+        tagName,
+        propDefaults: {
+            ...propDefaults,
+            val: false,
+            echoVal: false,
+            lhs: false,
+            rhs: true,
+            displayDelay: 16,
+            op: '===',
+            checkIfNonEmptyArray: false,
+            ifMediaMatches: '',
+            hiddenStyle: 'display:none',
+            toggleDisabled: false,
+        },
+        propInfo: {
+            ...propInfo,
+            val: {
+                notify: {
+                    echoTo: {
+                        key: 'echoVal',
+                        delay: 'displayDelay'
+                    } as IEchoTo<AP>
+                }
+            }
+        },
+        actions: {
+            calcVal: {
+                ifKeyIn: ['lhs', 'rhs']
+            },
+            onTrue: {
+                ifEquals: ['val', 'echoVal'],
+                ifAllOf: ['val']
+            },
+            onFalse: {
+                ifEquals: ['val', 'echoVal'],
+                ifNoneOf: ['val']
+            },
+            addMediaListener: {
+                ifKeyIn:  ['ifMediaMatches']
+            }
+        }
+    },
+    superclass: BeSwitched
+});
+
+register(ifWantsToBe, upgrade, tagName);
