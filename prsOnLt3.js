54a165669683f023eb3d7abbe7230235f0394197
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-03-17 12:20:40 -0400

    Update README.md

--- prsOnLt3.js
+++ prsOnLt3.js
@@ -10,24 +10,6 @@ const lhsPerimeterLhsOpRhsPerimeterRhs = String.raw `${lhsPerimeter}${lhsTypeLHS
 const lhsPerimeterLhsOpRhs = String.raw `${lhsPerimeter}${lhsTypeLHSProp}${op}${rhsTypeRhsProp}`;
 const lhsOpRhsPerimeterRhs = String.raw `${lhsTypeLHSProp}${op}${rhsPerimeter}${rhsTypeRhsProp}`;
 const lhsPartOpRhsPart = String.raw `(?<lhsPart>.*)${op}(?<rhsPart>.*)`;
-// const reTwoValSwitchStatements: RegExpOrRegExpExt<TwoValueSwitch>[] = [
-//     {
-//         regExp: new RegExp(`^when${lhsPerimeterLhsOpRhsPerimeterRhs}`),
-//         defaultVals:{}
-//     },
-//     {
-//         regExp: new RegExp(`^when${lhsPerimeterLhsOpRhs}`),
-//         defaultVals: {}
-//     },
-//     {
-//         regExp: new RegExp(`^when${lhsOpRhsPerimeterRhs}`),
-//         defaultVals: {}
-//     },
-//     {
-//         regExp: new RegExp(`^when${lhsOpRhs}`),
-//         defaultVals:{}
-//     },
-// ]
 const reTwoPartStatements = [
     {
         regExp: new RegExp(String.raw `^when${lhsPartOpRhsPart}`),
@@ -58,34 +40,6 @@ export async function prsOnLt3(self, negate = false) {
     const twoValueSwitches = [];
     const onUnion = negate ? [...(Off || []), ...(off || [])] : [...(On || []), ...(on || [])];
     for (const onS of onUnion) {
-        // const twoValSwitchTest = tryParse(onS, reTwoValSwitchStatements) as TwoValueSwitch;
-        // if(twoValSwitchTest !== null){
-        //     let {lhsProp, rhsProp, op} = twoValSwitchTest;
-        //     if(op === 'eq') twoValSwitchTest.op = 'equals';
-        //     const lhsEventSplit = lhsProp?.split('::');
-        //     if(lhsEventSplit?.length === 2){
-        //         lhsProp = twoValSwitchTest.lhsProp = lhsEventSplit[0];
-        //         twoValSwitchTest.lhsEvent = lhsEventSplit[1];
-        //     }
-        //     if(lhsProp?.includes(':')){
-        //         const split = lhsProp.split(':');
-        //         twoValSwitchTest.lhsProp = split[0];
-        //         twoValSwitchTest.lhsSubProp = '.' + split.slice(1).join('.');
-        //     }
-        //     const rhsEventSplit = rhsProp?.split('::');
-        //     if(rhsEventSplit?.length === 2){
-        //         rhsProp = twoValSwitchTest.rhsProp = rhsEventSplit[0];
-        //         twoValSwitchTest.rhsEvent = rhsEventSplit[1];
-        //     }
-        //     if(rhsProp?.includes(':')){
-        //         const split = rhsProp.split(':');
-        //         twoValSwitchTest.rhsProp = split[0];
-        //         twoValSwitchTest.rhsSubProp = '.' + split.slice(1).join('.');
-        //     }
-        //     twoValSwitchTest.negate = negate;
-        //     twoValueSwitches.push(twoValSwitchTest);
-        //     continue;
-        // }
         const twoPartStatementTest = tryParse(onS, reTwoPartStatements);
         if (twoPartStatementTest !== null) {
             const { lhsPart, rhsPart, op } = twoPartStatementTest;
--- prsOnLt3.ts
+++ prsOnLt3.ts
@@ -23,26 +23,6 @@ const lhsOpRhsPerimeterRhs = String.raw `${lhsTypeLHSProp}${op}${rhsPerimeter}${
 
 const lhsPartOpRhsPart = String.raw `(?<lhsPart>.*)${op}(?<rhsPart>.*)`;
 
-// const reTwoValSwitchStatements: RegExpOrRegExpExt<TwoValueSwitch>[] = [
-//     {
-//         regExp: new RegExp(`^when${lhsPerimeterLhsOpRhsPerimeterRhs}`),
-//         defaultVals:{}
-//     },
-//     {
-//         regExp: new RegExp(`^when${lhsPerimeterLhsOpRhs}`),
-//         defaultVals: {}
-//     },
-//     {
-//         regExp: new RegExp(`^when${lhsOpRhsPerimeterRhs}`),
-//         defaultVals: {}
-//     },
-//     {
-//         regExp: new RegExp(`^when${lhsOpRhs}`),
-//         defaultVals:{}
-//     },
-
-// ]
-
 const reTwoPartStatements: RegExpOrRegExpExt<TwoPartOpStatement>[] = [
     {
         regExp: new RegExp(String.raw `^when${lhsPartOpRhsPart}`),
@@ -75,34 +55,6 @@ export async function prsOnLt3(self: AP, negate = false) : ProPAP{
     const twoValueSwitches : Array<TwoValueSwitch> = [];
     const onUnion = negate ? [...(Off || []), ...(off || [])] : [...(On || []), ...(on || [])];
     for(const onS of onUnion){
-        // const twoValSwitchTest = tryParse(onS, reTwoValSwitchStatements) as TwoValueSwitch;
-        // if(twoValSwitchTest !== null){
-        //     let {lhsProp, rhsProp, op} = twoValSwitchTest;
-        //     if(op === 'eq') twoValSwitchTest.op = 'equals';
-        //     const lhsEventSplit = lhsProp?.split('::');
-        //     if(lhsEventSplit?.length === 2){
-        //         lhsProp = twoValSwitchTest.lhsProp = lhsEventSplit[0];
-        //         twoValSwitchTest.lhsEvent = lhsEventSplit[1];
-        //     }
-        //     if(lhsProp?.includes(':')){
-        //         const split = lhsProp.split(':');
-        //         twoValSwitchTest.lhsProp = split[0];
-        //         twoValSwitchTest.lhsSubProp = '.' + split.slice(1).join('.');
-        //     }
-        //     const rhsEventSplit = rhsProp?.split('::');
-        //     if(rhsEventSplit?.length === 2){
-        //         rhsProp = twoValSwitchTest.rhsProp = rhsEventSplit[0];
-        //         twoValSwitchTest.rhsEvent = rhsEventSplit[1];
-        //     }
-        //     if(rhsProp?.includes(':')){
-        //         const split = rhsProp.split(':');
-        //         twoValSwitchTest.rhsProp = split[0];
-        //         twoValSwitchTest.rhsSubProp = '.' + split.slice(1).join('.');
-        //     }
-        //     twoValSwitchTest.negate = negate;
-        //     twoValueSwitches.push(twoValSwitchTest);
-        //     continue;
-        // }
         const twoPartStatementTest = tryParse(onS, reTwoPartStatements) as TwoPartOpStatement;
         if(twoPartStatementTest !== null){
             const {lhsPart, rhsPart, op} = twoPartStatementTest;
