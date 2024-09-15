2a6d25825bfec96e7589a6e1f5346964a8776c60
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-09-15 04:29:22 -0400

    Merge branch 'baseline' of https://github.com/bahrus/be-switched into baseline

--- be-switched.ts
+++ be-switched.ts
@@ -1,8 +1,8 @@
 import {config as beCnfg} from 'be-enhanced/config.js';
 import {BE, BEConfig} from 'be-enhanced/BE.js';
 import {Actions, AllProps, AP, PAP, ProPAP} from './types';
-import { Positractions, PropInfo } from 'trans-render/froop/types';
-import {IEnhancement,  BEAllProps, EnhancementInfo} from 'trans-render/be/types';
+import { Positractions, PropInfo } from './ts-refs/trans-render/froop/types';
+import {IEnhancement,  BEAllProps, EnhancementInfo} from './ts-refs/trans-render/be/types';
 
 let cnt = 0;
 class BeSwitched extends BE<AP, Actions, HTMLTemplateElement> implements Actions{
--- 🎚️.js
+++ 🎚️.js
@@ -1,5 +1,8 @@
 import { MountObserver, seed, BeHive } from 'be-hive/be-hive.js';
-import { emc as baseEMC } from './behivior.void';
+import { emc as baseEMC } from './behivior.js';
+import {Registry} from 'be-hive/Registry.js';
+import {aggs} from 'be-hive/aggEvt.js';
+
 export const emc = {
     ...baseEMC,
     base: '🎚️',
@@ -7,3 +10,26 @@ export const emc = {
 };
 const mose = seed(emc);
 MountObserver.synthesize(document, BeHive, mose);
+
+for(const key in aggs){
+    Registry.register(emc, key, aggs[key]);
+}
+
+/**
+ * 
+ * @param {string} handlerName 
+ * @param {EventListenerOrFn} handler 
+ */
+export function register(handlerName, handler){
+    Registry.register(emc, handlerName, handler);
+}
+
+/**
+ * 
+ * @param {CSSQuery} q 
+ * @param {string} handlerName 
+ * @param {EventListenerOrFn} handler 
+ */
+export function within(q, handlerName, handler){
+    Registry.within(emc, q, handlerName, handler);
+}
