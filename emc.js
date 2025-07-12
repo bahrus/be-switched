2c5caefe978c4d231aea59cdfb185734028bce40
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-12 19:01:29 -0400

    Update README.md

--- emc.js
+++ emc.js
@@ -9,7 +9,7 @@ import {aggs} from 'be-hive/aggEvt.js';
 const op = String.raw `(?<!\\)(?<op>(equals|eq|lt|gt))`;
 const whenLHSPart = String.raw `when (?<lhsPart>.*)`;
 const rhsPart = String.raw `(?<rhsPart>.*)`;
-const onOrOffWhenLhsPartOpRhsPart = String.raw `^(?<onOrOff>(on|On|off|Off)) ${whenLHSPart} ${op} ${rhsPart}`;
+const onOrOffWhenLhsPartOpRhsPart = String.raw `^(?<onOrOff>(on|On|off|Off)) ${whenLHSPart} ${op} ${rhsPart}(, and set itemscope to ())?`;
 const onOrOffWhenLhsPartOpRhsPartWithin = String.raw `${onOrOffWhenLhsPartOpRhsPart}( w/i (?<within>.*))?`;
 const onIfRegisteredHandlerBasedOnDependencies = String.raw `^on if (?<registeredHandler>.*)\, based on (?<dependencyPart>.*)`;
 const onBasedOnDependencies = String.raw `^(o|O)n based on (?<dependencyPart>.*)`;
@@ -33,11 +33,7 @@ export const emc = {
                         defaultVals: {},
                         dssKeys: [['lhsPart', 'lhsSpecifier'], ['rhsPart', 'rhsSpecifier'], ['within', 'withinSpecifier']]
                     },
-                    // {
-                    //     regExp: onOrOffWhenLhsPartOpRhsPart,
-                    //     defaultVals: {},
-                    //     dssKeys: [['lhsPart', 'lhsSpecifier'], ['rhsPart', 'rhsSpecifier']]
-                    // },
+
                 ],
                 nValueSwitches: [
                     {
